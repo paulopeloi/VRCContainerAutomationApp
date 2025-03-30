bacfc04a0a36fdc0a0d2451c22251fffe39c51d2
Paulo Peloi <68501028+paulopeloi@users.noreply.github.com>
2025-03-30 18:02:35 -0400

    remove debug consoles

--- VRCContainerAutomationApp/src/Services/WarehouseLocationService.cs
+++ VRCContainerAutomationApp/src/Services/WarehouseLocationService.cs
@@ -2,7 +2,6 @@
 using VRCContainerAutomationApp.Database;
 using VRCContainerAutomationApp.SQLs;
 using VRCContainerAutomationApp.Mappers;
-using System.Diagnostics;
 
 namespace VRCContainerAutomationApp.Services;
 
@@ -18,11 +17,6 @@ public static class WarehouseLocationService
             ["@weight"] = weightValue,
             ["@height"] = heightValue
         };
-
-        Debug.WriteLine(sql);
-        Debug.WriteLine(idTypeValue);
-        Debug.WriteLine(weightValue);
-        Debug.WriteLine(heightValue);
 
         var result = SQLiteService.ExecuteQuery(sql, parameters);
         if (result.Count == 0) return null;
